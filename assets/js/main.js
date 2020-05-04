d1d303daf36368c66a08857fe1cc4a3f9443e104
Jeffrowetull <jeffrowetull@gmail.com>
2020-05-04 14:56:07 -0700

    deleted extra photos

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,11 +63,11 @@ jQuery(document).ready(function($) {
     );
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    new GitHubCalendar("#github-graph", "IonicaBizau");
+    GitHubCalendar("#github-graph", "rowebyrowe");
     
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "mdo", selector: "#ghfeed" });
+    GitHubActivity.feed({ username: "rowebyrowe", selector: "#ghfeed", limit: 10 });
 
 
 });
